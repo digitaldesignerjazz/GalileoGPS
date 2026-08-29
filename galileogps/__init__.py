6b192422e9593673eb94b6af273317a377cfd941
digitaldesignerjazz <135103659+digitaldesignerjazz@users.noreply.github.com>
2026-08-29 19:58:04 +0200

    chore: export QZS4/QZS5 orbit slots

--- galileogps/__init__.py
+++ galileogps/__init__.py
@@ -3,6 +3,7 @@
 from .constellation import Constellation, beidou_prn, classify_sat, qzss_prn
 from .hybrid import build_fix
 from .nmea import parse_nmea_line, parse_nmea_stream
+from .qzss_orbits import QZS4, QZS5, get_slot
 from .region import qzss_in_service_area
 
 __all__ = [
@@ -11,9 +12,12 @@ __all__ = [
     "beidou_prn",
     "qzss_prn",
     "qzss_in_service_area",
+    "QZS4",
+    "QZS5",
+    "get_slot",
     "parse_nmea_line",
     "parse_nmea_stream",
     "build_fix",
 ]
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
